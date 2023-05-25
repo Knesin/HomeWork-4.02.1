b09bfa7b5771f16000250ec39431a240e2fa1f6b
Maksim Kl <GuruHacker@yandex.ru>
2023-05-25 22:16:58 +0300

    Fix error

--- HomeWork-4.02.1.cpp
+++ HomeWork-4.02.1.cpp
@@ -6,26 +6,27 @@ struct point {
 	double m_y;
 	point(double x, double y) {
 		m_x = x;
-		y = y;
+		m_y = y; //изменено y -> m_y
 	}
-}
+}; //поставлена  ;
 
 void print_point(const point& point_object) {
 	std::cout << "x:" << point_object.m_x << ", y: "
 		<< point_object.m_y << std::endl;
 }
 
-int Main()
+int main() // изменено Main -> main
 {
 	int i;
-	for (i = 0; i < 5; i++); {
+	for (i = 0; i < 5; i++) { // убрана ; между ) {
 		point my_point(i, 2 * i);
 		print_point(my_point);
 	}
 	return 0;
 }
 
-void print_point(const point& point_object) {
-	std::cout << "x:" << point_object.m_x << ", y: "
-		<< point_object.m_y << std::endl;
-}
+//Не запустится, т.к. описание ниже вызова.
+//void print_point(const point& point_object) {
+//	std::cout << "x:" << point_object.m_x << ", y: "
+//		<< point_object.m_y << std::endl;
+//}
